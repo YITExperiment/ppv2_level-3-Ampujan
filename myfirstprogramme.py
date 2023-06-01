565c9dfe829657634391e93eec2acfc872420223
Ampujan <135000085+Ampujan@users.noreply.github.com>
2023-06-01 22:22:22 +0530

    Add files via upload

---
+++ myfirstprogramme.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/Ampujan/Desktop/myfirstprogramme.py ============
+Hello, World
+What is your name?Ampujan
+Hello Ampujan
+>>> age=14
+>>> print(age)
+14
+>>> weight=42
+>>> print(weight)
+42
+>>> height=144
+>>> print(height)
+144
